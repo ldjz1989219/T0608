cd87b118fe956d3c17a00481770eabe82cbf810b
ldjz1989219 <806626709@qq.com>
2017-04-03 00:54:36 +0800

    Update Gitdemo02.java
    服务器修改

--- src/com/atguigu/github/Gitdemo02.java
+++ src/com/atguigu/github/Gitdemo02.java
@@ -5,6 +5,8 @@ public class Gitdemo02 {
 	public static void main(String[] args) {
 		System.out.println("hello github");
 		System.out.println("hello 0809");
+		System.out.println("hello 1010");
+		
 	}
 
 }
